49d1439c366d832d12a8028b4e68d4d5f4acddcd
hy2510 <hy2510@gmail.com>
2021-09-03 10:47:18 +0900

    2021-09-03_1
    common.js 누락

--- student/assets/js/common.js
+++ student/assets/js/common.js
@@ -7,14 +7,48 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-//스마트폰 서브메뉴
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function () {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").style.top = "60px";
-    } else {
-        document.getElementById("navbar").style.top = "0px";
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//     var currentScrollPos = window.pageYOffset;
+//     if (prevScrollpos > currentScrollPos) {
+//         document.getElementById("navbar").style.top = "60px";
+//     } else {
+//         document.getElementById("navbar").style.top = "0px";
+//     }
+//     prevScrollpos = currentScrollPos;
+// }
+
+//스마트폰 하단 메뉴 애니메이션
+var lastScrollTop = 0;
+var delta = 5;
+var fixBox = document.getElementById('mPubMenu');
+var fixBoxHeight = fixBox.offsetHeight;
+var didScroll;
+
+window.onscroll = function (e) {
+    didScroll = true;
+};
+
+setInterval(function () {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
     }
-    prevScrollpos = currentScrollPos;
-}
+}, 250);
+
+function hasScrolled() {
+    var nowScrollTop = window.scrollY;
+    if (Math.abs(lastScrollTop - nowScrollTop) <= delta) {
+        return;
+    }
+    if (nowScrollTop > lastScrollTop && nowScrollTop > fixBoxHeight) {
+        //Scroll down
+        $('#mPubMenu').slideUp();
+    } else {
+        if (nowScrollTop + window.innerHeight < document.body.offsetHeight) {
+            //Scroll up
+            $('#mPubMenu').slideDown();
+        }
+    }
+    lastScrollTop = nowScrollTop;
+};
